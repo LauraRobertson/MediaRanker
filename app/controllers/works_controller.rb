1c617b592430fa984321a608b41de8b1399b35c5
Laura Robertson <mslaura.mae@gmail.com>
2017-10-10 16:51:26 -0700

    Modified and checked adding a new work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,14 +1,23 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all
   end
 
   def show
+    # @work = Work.find_by()
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new work_params
+    if @work.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +27,11 @@ class WorksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end

5e4e09fbf18a64787ce1f38ed16af2368e2741e4
Laura Robertson <mslaura.mae@gmail.com>
2017-10-10 17:17:34 -0700

    Tests for add new work, presence and uniqueness (category, title

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,4 @@
 class Work < ApplicationRecord
+  validates :category, presence: true
+  validates :title, presence: true, uniqueness: true
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,29 @@ require "test_helper"
 
 describe Work do
   let(:work) { Work.new }
+  let(:gatsby) { works(:gatsby) }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "must have a category" do
+    work.title = "Title"
+
+    work.valid?.must_equal false
+    work.category = "Category"
+    work.valid?.must_equal true
+  end
+
+  it "must have a title" do
+    work.category = "Category"
+
+    work.valid?.must_equal false
+    work.title = "Title"
+    work.valid?.must_equal true
+  end
+
+  it "must have a unique title" do
+    work.title = "The Great Gatsby"
+    work.category = "Category"
+    work.valid?.must_equal false
+    work.title = "The Greater Gatsby"
+    work.valid?.must_equal true
   end
 end

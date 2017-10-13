2b5c1193c1cacbe5398678fbd2ffbaa57d51c942
Laura Robertson <mslaura.mae@gmail.com>
2017-10-12 22:02:35 -0700

    Existing user can succesfully log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,17 @@ class UsersController < ApplicationController
   end
 
   def login
+    user = User.find_by(username: params[:username])
+
+    if user
+      flash[:success] = "#{ user.username } is successfully logged in"
+      session[:username] = user.id
+      redirect_to root_path
+    else
+      # flash[:success] = "Successfully created new user #{ params[:username] } with ID #{ params[:id] }"
+      # session[:username] = user.username
+      # redirect_to root_path
+    end
   end
 
 end

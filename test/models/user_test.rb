708aa0221a3b9dc0774bb6cc0429e3822aade0b3
Laura Robertson <mslaura.mae@gmail.com>
2017-10-13 15:26:26 -0700

    User model tests for presence and uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe User do
-  # let(:user) { User.new }
-  #
-  # it "must be valid" do
-  #   value(user).must_be :valid?
-  # end
+  let(:user) { User.new }
+  let(:user1) { users(:user1) }
+
+  it "must be valid" do
+    user1.must_be :valid?
+  end
+
+  it "must have a username" do
+    user.username = "   "
+    user.valid?.must_equal false
+
+    user.username = "Name"
+    user.valid?.must_equal true
+  end
+
+  it "must be unique" do
+    user1.username.must_equal "rainbow"
+    user.username = "rainbow"
+    user.valid?.must_equal false
+  end
 end

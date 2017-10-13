c95d0d3daf53af57e82c8d9fcb99c69a0ad3e8b9
Laura Robertson <mslaura.mae@gmail.com>
2017-10-13 15:35:41 -0700

    Tests for user view routes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,34 @@
 require "test_helper"
 
 describe UsersController do
+  let(:user1) { users(:user1) }
 
+  describe "view pages work" do
+    it "goes to the index users page" do
+      get users_path
+      must_respond_with :success
+    end
+
+    it "goes to the show users page" do
+      get user_path(user1.id)
+      must_respond_with :success
+    end
+
+    it "goes to the login form page" do
+      get login_path
+      must_respond_with :success
+    end
+  end
+
+  describe "a user can create a new account" do
+
+  end
+
+  describe "a user can re-login" do
+
+  end
+
+  describe "a user can log out" do
+
+  end
 end

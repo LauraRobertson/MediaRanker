93789a0379856907712020705461c37e66c668c8
Laura Robertson <mslaura.mae@gmail.com>
2017-10-13 09:35:42 -0700

    Minor html changes, controller tests reorganized

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -4,52 +4,60 @@ class WorksControllerTest < ActionDispatch::IntegrationTest
   describe "WorksController" do
     let(:gatsby) { works(:gatsby) }
 
-    it "going to the home page is successful" do
-      get root_path
-      must_respond_with :success
+    describe "view pages work" do
+      it "going to the home page is successful" do
+        get root_path
+        must_respond_with :success
+      end
+
+      it "going to the index page is successful" do
+        get works_path
+        must_respond_with :success
+      end
+
+      it "going to the add work page is successful" do
+        get new_work_path
+        must_respond_with :success
+      end
     end
 
-    it "going to the index page is successful" do
-      get works_path
-      must_respond_with :success
+    describe "a users can create a work" do
+      it "should be able to create a work" do
+        proc {
+          post works_path, params: { work: { category: "book", title: "New Title"}}
+        }.must_change 'Work.count', 1
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+
+      it "should re-render form if can't create work" do
+        proc { post works_path, params: { work: { category: "book", title: "  "}}}.must_change 'Work.count', 0
+        must_respond_with :success
+      end
     end
 
-    it "going to the add work page is successful" do
-      get new_work_path
-      must_respond_with :success
+    describe "a users can edit a work" do
+      it "should be able to successfully edit a work" do
+        put work_path( works(:gatsby)), params: {work: {title: "Updated Title"}}
+
+        updated_work = Work.find( works(:gatsby).id)
+        updated_work.title.must_equal "Updated Title"
+        must_redirect_to works_path
+      end
+
+      it "should be re-render if it can't edit a work" do
+        put work_path( works(:gatsby)), params: {work: {title: "   "}}
+        must_respond_with :success
+      end
     end
 
-    it "should be able to create a work" do
-      proc {
-        post works_path, params: { work: { category: "book", title: "New Title"}}
-      }.must_change 'Work.count', 1
-      must_respond_with :redirect
-      must_redirect_to root_path
+    describe "a user can delete a work" do
+      it "successfully deletes a work" do
+        proc {(delete work_path(gatsby.id))}.must_change 'Work.count', -1
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
     end
 
-    it "should re-render form if can't create work" do
-      proc { post works_path, params: { work: { category: "book", title: "  "}}}.must_change 'Work.count', 0
-      must_respond_with :success
-    end
-
-    it "should be able to successfully edit a work" do
-      put work_path( works(:gatsby)), params: {work: {title: "Updated Title"}}
-
-      updated_work = Work.find( works(:gatsby).id)
-      updated_work.title.must_equal "Updated Title"
-      must_redirect_to works_path
-    end
-
-    it "should be re-render if it can't edit a work" do
-      put work_path( works(:gatsby)), params: {work: {title: "   "}}
-      must_respond_with :success
-    end
-
-    it "successfully deletes a work" do
-      proc {(delete work_path(gatsby.id))}.must_change 'Work.count', -1
-      must_respond_with :redirect
-      must_redirect_to root_path
-    end
-  end
-
-end
+  end # main describe
+end #class

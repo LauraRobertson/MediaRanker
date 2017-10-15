163b66021d4254a6d3d1ada7963ecdb4133f9f0a
Laura Robertson <mslaura.mae@gmail.com>
2017-10-15 15:33:30 -0700

    Vote model tests: validations and relationships

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:user) { User.first }
+  let(:work) { Work.first }
+  let(:vote) { Vote.new(work: work, user: user) }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  describe "validations" do
+    it "must be valid" do
+      value(vote).must_be :valid?
+    end
+
+    it "needs a user_id" do
+      test = Vote.new(work: work)
+      test.wont_be :valid?
+    end
+
+    it "needs a work_id" do
+      test = Vote.new(user: user)
+      test.wont_be :valid?
+    end
+  end
+
+  describe "its relationships to works/users" do
+    it "talks to work" do
+      vote.must_respond_to :work
+    end
+
+    it "talks to user" do
+      vote.must_respond_to :user
+    end
   end
 end

01bec801d97a98c41924f1dfb4f41ce1979bd82c
Laura Robertson <mslaura.mae@gmail.com>
2017-10-15 16:52:33 -0700

    Controller test for session logout

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -25,17 +25,26 @@ describe UsersController do
       proc { post login_path, params: {username: "rainbow"}}.must_change 'User.count', 0
       must_respond_with :redirect
       must_redirect_to root_path
-      assert_equal "rainbow is successfully logged in", flash[:success]
+      flash[:success].must_equal "rainbow is successfully logged in"
+      session[:username].must_equal user1.id
     end
 
     it "can add a new user" do
       proc { post login_path, params: { username: "mnajk"}}.must_change 'User.count', 1
       must_respond_with :redirect
       must_redirect_to users_path
-      assert_equal "Successfully created new user mnajk with ID 206669144", flash[:success]
+      flash[:success].must_equal "Successfully created new user mnajk with ID 206669144"
+      session[:username].must_equal User.last.id
     end
   end
 
-  # describe "a user can log out" do
-  # end
+  describe "a user can log out" do
+    it "log out works correctly" do
+      post login_path, params: {username: "rainbow"}
+      session[:username].wont_be_nil
+
+      delete logout_path
+      session[:username].must_be_nil
+    end
+  end
 end

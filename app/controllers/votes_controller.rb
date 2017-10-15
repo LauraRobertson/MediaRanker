1c08f73b0b322108b448e42a1c2612208855364d
Laura Robertson <mslaura.mae@gmail.com>
2017-10-14 21:18:34 -0700

    Vote controller create written

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,26 @@
 class VotesController < ApplicationController
 
   def create
+    vote = Vote.new(vote_params)
+    voting_user = session[:username]
+
+    if vote.save
+      flash[:message] = "Successfully upvoted!"
+    else
+      if voting_user == nil
+        flash[:message] = "You must be logged in to do that!"
+      else
+        flash[:message] = "Hey! You've already voted for that!"
+      end
+    end
+
+    redirect_to works_path
+  end
+
+  private
+
+  def vote_params
+    params.permit(:work_id)
   end
 
 end

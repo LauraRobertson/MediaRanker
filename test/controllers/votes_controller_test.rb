34f312f9cc0dfa4c40b20e7979d9fbde137d54fe
Laura Robertson <mslaura.mae@gmail.com>
2017-10-15 17:45:40 -0700

    Controller test for error handling

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -7,17 +7,20 @@ describe VotesController do
     flash[:success].must_equal "rainbow is successfully logged in"
   end
 
-  # it "can create a vote" do
-  #   proc { post create_vote_path(Work.first.id) }.must_change 'Vote.count', 1
-  #   flash[:success].must_equal "Successfully upvoted!"
-  # end
-  #
+  it "can create a vote" do
+    Vote.first.destroy
+    proc { post create_vote_path(Work.first.id) }.must_change 'Vote.count', 1
+    flash[:success].must_equal "Successfully upvoted!"
+  end
+
   it "prevents multiple voting from the same user" do
     proc { post create_vote_path(Work.first.id) }.must_change 'Vote.count', 0
     flash[:error].must_equal "Hey! You've already voted for that!"
   end
 
-  # it "requires the user to be logged in to vote" do
-  #   flash[:error].must_equal "You must be logged in to do that!"
-  # end
+  it "requires the user to be logged in to vote" do
+    delete logout_path
+    post create_vote_path(Work.first.id)
+    flash[:error].must_equal "You must be logged in to do that!"
+  end
 end
